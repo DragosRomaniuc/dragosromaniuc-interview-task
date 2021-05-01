6cbb6250074dd19239749b406bfb38b4a77624e4
DragosRomaniucStint <dragos.romaniuc@stint.co>
2021-05-01 16:03:05 +0300

    Remove unused variables / files

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,16 +1,9 @@
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './modules/database/DatabaseModule';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import config from './configs/config/config';
-import envValidation from './configs/env/env.validation';
-import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { User } from './users/schemas/user.schema';
-import { AppModule } from './app.module';
-// jest.setTimeout(30000);
+
 describe('AppController', () => {
   let appController: AppController;
 
--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,14 +1,7 @@
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '../modules/database/DatabaseModule';
 import { Test, TestingModule } from '@nestjs/testing';
 import { User } from './schemas/user.schema';
-import { UsersModule } from './users.module';
 import { UsersService } from './users.service';
-import config from '../configs/config/config';
-import envValidation from '../configs/env/env.validation';
-import { AppModule } from '../app.module';
 import { getModelToken } from '@nestjs/mongoose';
-jest.setTimeout(10000);
 
 describe('UsersService', () => {
   let service: UsersService;
